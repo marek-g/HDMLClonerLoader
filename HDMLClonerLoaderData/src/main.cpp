18e3c9dbb06b50ab787bc714b5d70b5f0c55f872
Marek G <no@mail.com>
2019-01-28 12:23:11 +0100

    Updated to HDMLCloner 2.40.409.

--- HDMLClonerLoaderData/src/main.cpp
+++ HDMLClonerLoaderData/src/main.cpp
@@ -9,28 +9,38 @@ static void Init()
 }
 
 
+// WindowProc
+
+typedef int(__fastcall *window_proc)(int self, void *notUsed, unsigned int wParam, unsigned int uMsg, unsigned int lParam);
+window_proc _window_proc = (window_proc)0x43B9C0;
+
+
 // window_layout
 
 typedef int(__fastcall *window_layout)(int self, void *notUsed, signed int width, int height);
-window_layout _window_layout = (window_layout)0x438770;
+window_layout _window_layout = (window_layout)0x43A1C0;
+
 
 
 // helper methods
 
-typedef int(__fastcall *sub_420C70)(int self, void *notUsed, int a2, int a3, int a4, int a5);
-sub_420C70 _sub_420C70 = (sub_420C70)0x420C70;
+typedef int(__fastcall *sub_421090)(int self, void *notUsed, int a2, int a3, int a4, int a5);
+sub_421090 _sub_421090 = (sub_421090)0x421090;
 
-typedef int(__fastcall *sub_42C3D0)(int self, void *notUsed, int a2);
-sub_42C3D0 _sub_42C3D0 = (sub_42C3D0)0x42C3D0;
+typedef int(__fastcall *sub_42D6E0)(int self, void *notUsed, int a2);
+sub_42D6E0 _sub_42D6E0 = (sub_42D6E0)0x42D6E0;
 
-typedef int(__fastcall *sub_429300)(int self, void *notUsed);
-sub_429300 _sub_429300 = (sub_429300)0x429300;
+typedef int(__fastcall *sub_42D5C0)(int self, void *notUsed, int a2, int a3, int a4, int a5, int a6);
+sub_42D5C0 _sub_42D5C0 = (sub_42D5C0)0x42D5C0;
 
-typedef int(__fastcall *sub_4282F0)(int self, void *notUsed);
-sub_4282F0 _sub_4282F0 = (sub_4282F0)0x4282F0;
+typedef int(__fastcall *sub_429CF0)(int self, void *notUsed);
+sub_429CF0 _sub_429CF0 = (sub_429CF0)0x0429CF0;
 
-typedef BOOL(*sub_42E6F0)();
-sub_42E6F0 _sub_42E6F0 = (sub_42E6F0)0x42E6F0;
+typedef int(__fastcall *sub_428CE0)(int self, void *notUsed);
+sub_428CE0 _sub_428CE0 = (sub_428CE0)0x428CE0;
+
+typedef BOOL(*sub_42FA80)();
+sub_42FA80 _sub_42FA80 = (sub_42FA80)0x42FA80;
 
 
 //
@@ -38,11 +48,25 @@ sub_42E6F0 _sub_42E6F0 = (sub_42E6F0)0x42E6F0;
 //
 
 typedef void(__fastcall *cwnd_move_window)(int self, void *notUsed, int x, int y, int width, int height, BOOL repaint);
-cwnd_move_window _cwnd_move_window = (cwnd_move_window)0x43FAAA; //0x44C6E0;
+cwnd_move_window _cwnd_move_window = (cwnd_move_window)0x441A2A; // ?MoveWindow@CWnd@@QAEXHHHHH@Z MFC42
 
 typedef BOOL(__fastcall *cwnd_show_window)(int self, void *notUsed, int cmdShow);
-cwnd_show_window _cwnd_show_window = (cwnd_show_window)0x43FA98; //0x44C6EC;
+cwnd_show_window _cwnd_show_window = (cwnd_show_window)0x441A18; // ?ShowWindow@CWnd@@QAEHH@Z MFC42
 
+
+//
+// __window_proc
+//
+
+int __fastcall __window_proc(int self, void *notUsed, unsigned int wParam, unsigned int uMsg, unsigned int lParam)
+{
+	return _window_proc(self, notUsed, wParam, uMsg, lParam);
+}
+
+
+//
+// __window_layout
+//
 
 int __fastcall __window_layout(int self, void *notUsed, signed int width, int height)
 {
@@ -50,140 +74,83 @@ int __fastcall __window_layout(int self, void *notUsed, signed int width, int he
 	int v5; // edi@10
 	int v6; // edi@15
 
-	result = *(DWORD *)(self + 3000);
+	result = *(DWORD *)(self + 3232);
 	if (!result)
 	{
-		result = *(DWORD *)(self + 2736);
+		result = *(DWORD *)(self + 2936);
 		if (result)
 		{
-			// MAREK BEGIN
-			_cwnd_move_window((self + 1296), 0, 7, 7, width - 13, height - 13, 1);
-			_cwnd_move_window((self + 2904), 0, 7, 7, width - 13, height - 13, 1);
-			_cwnd_show_window((self + 2704), 0, 0);
-			_cwnd_show_window((self + 696), 0, 0);
-			_cwnd_show_window((self + 896), 0, 0);
-			_cwnd_show_window((self + 1096), 0, 0);
-			_cwnd_show_window((self + 1704), 0, 0);
-			_cwnd_show_window((self + 96), 0, 0);
-			_cwnd_show_window((self + 1904), 0, 0);
-			_cwnd_show_window((self + 496), 0, 0);
-			_cwnd_show_window((self + 2504), 0, 0);
-			_cwnd_show_window((self + 2104), 0, 0);
-			_cwnd_show_window((self + 296), 0, 0);
-			_cwnd_show_window((self + 2304), 0, 0);
-			return result;
-			// MAREK END
-
-
-			// window close button
-			_cwnd_move_window((self + 2704), 0, width - 25, 5, 13, 13, 1);
-
-			// window minimize button
-			_cwnd_move_window((self + 696), 0, width - 50, 5, 13, 13, 1);
-
-			// window help button
-			_cwnd_move_window((self + 896), 0, width - 75, 5, 13, 13, 1);
-
-			// window about (info) button
-			_cwnd_move_window((self + 1096), 0, width - 100, 5, 13, 13, 1);
-
-			if (*(DWORD *)(self + 2936))
+			_cwnd_move_window((self + 2904), 0, width - 25, 5, 13, 13, 1);
+			_cwnd_move_window((self + 896), 0, width - 50, 5, 13, 13, 1);
+			_cwnd_move_window((self + 1096), 0, width - 75, 5, 13, 13, 1);
+			_cwnd_move_window((self + 1296), 0, width - 100, 5, 13, 13, 1);
+			if (*(DWORD *)(self + 3136))
 			{
-				//OutputDebugString(L"1");
-
-				if (*((void**)0x5229D4))
+				if (*((void**)0x525A1C))
 				{
-					//OutputDebugString(L"2");
-
-					_sub_420C70((int)*((void**)0x5229D4), 0, 7, 30, width - 13, height - 100);
-					if (*((DWORD *)*((void**)0x5229D4) + 447) != 1)
-						_sub_42C3D0((self + 2904), 0, 0);
+					_sub_421090((int)*((void**)0x525A1C), 0, 7, 30, width - 13, height - 100);
+					if (!*((DWORD *)*((void**)0x525A1C) + 447))
+						_sub_42D6E0((self + 3104), 0, 0);
 				}
-
-				// video window
-				_cwnd_move_window((self + 1296), 0, 7, 30, width - 13, height - 100, 1);
-				_cwnd_move_window((self + 2904), 0, 7, 30, width - 13, height - 100, 1);
-
-				_cwnd_move_window((self + 1368), 0, width - 100, height - 35, 67, 12, 1);
-				_cwnd_move_window((self + 1504), 0, width - 30, height - 35, 13, 13, 1);
-				if (*(DWORD *)(self + 5408) && (signed int)*((WPARAM*)0x52248C) < 3)
+				_cwnd_move_window((self + 1496), 0, 7, 30, width - 13, height - 100, 1);
+				_sub_42D5C0((self + 3104), 0, 7, 30, width - 13, height - 100, 1);
+				_cwnd_move_window((self + 1568), 0, width - 100, height - 35, 67, 12, 1);
+				_cwnd_move_window((self + 1704), 0, width - 30, height - 35, 13, 13, 1);
+				if (*(DWORD *)(self + 5640) && *((signed int*)0x5254D4) < 3)
 				{
-					//OutputDebugString(L"3");
-
-					_cwnd_move_window((self + 1704), 0, width / 2 - 129, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 96), 0, width / 2 - 197, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 1904), 0, width / 2 - 265, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 496), 0, width / 2 - 61, height - 63, 56, 56, 1);
-					_cwnd_move_window((self + 2504), 0, width / 2 + 5, height - 63, 56, 56, 1);
-					_cwnd_move_window((self + 2104), 0, width / 2 + 91, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 296), 0, width / 2 + 159, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 2304), 0, width / 2 + 227, height - 50, 38, 38, 1);
-					_cwnd_show_window((self + 496), 0, 5);
+					_cwnd_move_window((self + 1904), 0, width / 2 - 129, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 296), 0, width / 2 - 197, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2104), 0, width / 2 - 265, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 96), 0, width / 2 - 333, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 696), 0, width / 2 - 61, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2704), 0, width / 2 + 5, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2304), 0, width / 2 + 91, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 496), 0, width / 2 + 159, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2504), 0, width / 2 + 227, height - 50, 38, 38, 1);
+					_cwnd_show_window((self + 696), 0, 5);
 				}
 				else
 				{
-					//OutputDebugString(L"4");
-
 					v5 = (width - 56) / 2;
-					
-					// record button
-					_cwnd_move_window((self + 2504), 0, v5, height - 63, 56, 56, 1);
-
-					// capture image button
-					_cwnd_move_window((self + 2104), 0, v5 + 86, height - 50, 38, 38, 1);
-					
-					// capture GIF button
-					_cwnd_move_window((self + 296), 0, v5 + 154, height - 50, 38, 38, 1);
-					
-					// settings button
-					_cwnd_move_window((self + 2304), 0, v5 + 222, height - 50, 38, 38, 1);
-					
-					// full screen button
-					_cwnd_move_window((self + 1704), 0, v5 - 68, height - 50, 38, 38, 1);
-					
-					// video broadcast button
-					_cwnd_move_window((self + 96), 0, v5 - 136, height - 50, 38, 38, 1);
-					
-					// file list button
-					_cwnd_move_window((self + 1904), 0, v5 - 204, height - 50, 38, 38, 1);
-					
-					// pause recording button
-					_cwnd_show_window((self + 496), 0, 0);
+					_cwnd_move_window((self + 2704), 0, v5, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2304), 0, v5 + 86, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 496), 0, v5 + 154, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2504), 0, v5 + 222, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 1904), 0, v5 - 68, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 296), 0, v5 - 136, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2104), 0, v5 - 204, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 96), 0, v5 - 272, height - 50, 38, 38, 1);
+					_cwnd_show_window((self + 696), 0, 0);
 				}
 			}
-			_sub_429300(*(int *)(self + 2992), 0);
-			_sub_4282F0(*(int *)(self + 2996), 0);
-			result = _sub_42E6F0();
+			_sub_429CF0(*(int *)(self + 3224), 0);
+			_sub_428CE0(*(int *)(self + 3228), 0);
+			result = _sub_42FA80();
 			if (result)
 			{
-				//OutputDebugString(L"5");
-
-				if (*(DWORD *)(self + 5408) && (signed int)*((WPARAM*)0x52248C) < 3)
+				if (*(DWORD *)(self + 5640) && *((signed int*)0x5254D4) < 3)
 				{
-					//OutputDebugString(L"6");
-
-					_cwnd_move_window((self + 1704), 0, width / 2 - 129, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 1904), 0, width / 2 - 197, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 496), 0, width / 2 - 61, height - 63, 56, 56, 1);
-					_cwnd_move_window((self + 2504), 0, width / 2 + 5, height - 63, 56, 56, 1);
-					_cwnd_move_window((self + 2104), 0, width / 2 + 91, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 2304), 0, width / 2 + 159, height - 50, 38, 38, 1);
-					_cwnd_show_window((self + 496), 0, 5);
+					_cwnd_move_window((self + 1904), 0, width / 2 - 129, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2104), 0, width / 2 - 197, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 696), 0, width / 2 - 61, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2704), 0, width / 2 + 5, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2304), 0, width / 2 + 91, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2504), 0, width / 2 + 159, height - 50, 38, 38, 1);
+					_cwnd_show_window((self + 696), 0, 5);
 				}
 				else
 				{
-					//OutputDebugString(L"7");
-
 					v6 = (width - 56) / 2;
-					_cwnd_move_window((self + 2504), 0, v6, height - 63, 56, 56, 1);
-					_cwnd_move_window((self + 2104), 0, v6 + 86, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 2304), 0, v6 + 154, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 1704), 0, v6 - 68, height - 50, 38, 38, 1);
-					_cwnd_move_window((self + 1904), 0, v6 - 136, height - 50, 38, 38, 1);
-					_cwnd_show_window((self + 496), 0, 0);
+					_cwnd_move_window((self + 2704), 0, v6, height - 63, 56, 56, 1);
+					_cwnd_move_window((self + 2304), 0, v6 + 86, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2504), 0, v6 + 154, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 1904), 0, v6 - 68, height - 50, 38, 38, 1);
+					_cwnd_move_window((self + 2104), 0, v6 - 136, height - 50, 38, 38, 1);
+					_cwnd_show_window((self + 696), 0, 0);
 				}
-				_cwnd_show_window((self + 96), 0, 0);
-				result = _cwnd_show_window((self + 296), 0, 0);
+				_cwnd_show_window((self + 296), 0, 0);
+				_cwnd_show_window((self + 496), 0, 0);
+				result = _cwnd_show_window((self + 96), 0, 0);
 			}
 		}
 	}
@@ -231,6 +198,7 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
         
 		Init();
 
+		DetourAttach(&(PVOID&)_window_proc, __window_proc);
 		DetourAttach(&(PVOID&)_window_layout, __window_layout);
 		//DetourAttach(&(PVOID&)_showHideButtons, __showHideButtons);
 		//DetourAttach(&(PVOID&)_sub1, __sub1);
@@ -247,7 +215,8 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
     else if (dwReason == DLL_PROCESS_DETACH) {
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        
+
+		DetourDetach(&(PVOID&)_window_proc, __window_proc);
 		DetourDetach(&(PVOID&)_window_layout, __window_layout);
 		//DetourDetach(&(PVOID&)_showHideButtons, __showHideButtons);
 		//DetourDetach(&(PVOID&)_sub1, __sub1);
